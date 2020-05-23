8c8db9d6a2141b2e836a60c0b0c1947501ae0e04
mike-baumgarten <32228925+mike-baumgarten@users.noreply.github.com>
2020-05-23 15:35:24 -0600

    hover works commented out header code for step 11

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 
-// Waits for page to load before running
+
 $(document).ready(function () {
-    
-    // Adds click function to id btnSubmit
-    $('#btnSubmit').click(function () {
-        // Grabs value from id textSubmit and alerts on it
-        alert($('#textSubmit').val());
+    let $div = $("<div>", { class: "textBox" });
+    $('body').append($div);
+    $('#btnSubmit').click(function (e) {
+        e.preventDefault()
+        let $text = $('#textSubmit').val();
+        // let $header2 = $("<h2>" + $text + "</h2>").addClass("header2");
+        // $('div.textBox').append($header2)
+        // $(".header2").mouseover(function () {
+        //     $(this).css({"background-color": "red", "border-radius": "10px"})
+        //     console.log('test')
+        // })
     })
     $('#textSubmit').on('input', function () {
         if ($('#textSubmit').val() === '') {
@@ -14,4 +20,8 @@ $(document).ready(function () {
             $('#btnSubmit').removeAttr('disabled')
         }
     });
+
+
+
 });
+
